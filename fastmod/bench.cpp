7103c1827957feccca51918e987819b904ba204c
Vladimir Ruzanov <ruzanov_va@nniirt.ru>
2017-07-27 10:37:06 +0300

    Compearing fastmod with mod

--- fastmod/bench.cpp
+++ fastmod/bench.cpp
@@ -11,6 +11,30 @@ static void generate_arg_pairs(benchmark::internal::Benchmark *b)
   }
 }
 
+static void bench_mod(benchmark::State& state)
+{
+  const int size = state.range(0);
+  assert(size >= 16 && "Only support 16 integers at a time!");
+  const int ceil = state.range(1);
+  std::vector<int> input, output;
+  input.resize(size, 0);
+  output.resize(size, 0);
+
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_int_distribution<int> dist(0, 255);
+  for (int& i : input) {
+    i = dist(rng);
+  }
+
+  while (state.KeepRunning()) {
+    for (int i = 0; i < size; ++i) {
+      output[i] = input[i] % ceil;
+    }
+  }
+}
+BENCHMARK(bench_mod)->Apply(generate_arg_pairs);
+
 static void bench_fastmod(benchmark::State& state)
 {
   const int size = state.range(0);
